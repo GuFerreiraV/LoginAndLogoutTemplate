0190da6abb2677bdcb6f1e50e13e9f632e6c950e
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-06-30 14:48:51 -0300

    lógica inicial da tela de login

--- LoginAndLogoutTemplate/Login.xaml.cs
+++ LoginAndLogoutTemplate/Login.xaml.cs
@@ -1,9 +1,52 @@
-namespace LoginAndLogoutTemplate;
-
-public partial class Login : ContentView
+namespace LoginAndLogoutTemplate
 {
-	public Login()
-	{
-		InitializeComponent();
-	}
+	public partial class Login : ContentPage
+    {
+		public Login()
+		{
+			InitializeComponent();
+		}
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                List<UserDatas> user_list = new List<UserDatas>()
+                {
+                    new UserDatas()
+                    {
+                        User = "jose",
+                        Password = "123"
+                    }
+                };
+
+                UserDatas datas = new UserDatas()
+                {
+                    User = txt_user.Text,
+                    Password = txt_password.Text
+                };
+
+                if (user_list.Any(i => datas.User == i.User &&
+                    datas.Password == i.Password
+                    ))
+                {
+                    await SecureStorage.Default.SetAsync("usuario_logado", datas.User);
+
+                    App.Current.MainPage = new Protegida();
+                }
+                else 
+                {
+                   throw new Exception("Usuário e/ou senha incorreta");
+                }
+
+            }
+            catch (Exception ex) 
+            {
+
+               await DisplayAlert("Ops", ex.Message, "Fechar");
+            
+            }
+        }
+    }
+
 }
