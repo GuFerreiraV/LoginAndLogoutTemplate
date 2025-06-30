2695bff7654148b0b0b492ac022e48b577b12026
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-06-30 13:46:15 -0300

    Add project files.

---
+++ LoginAndLogoutTemplate/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LoginAndLogoutTemplate
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LoginAndLogoutTemplate/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LoginAndLogoutTemplate
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
